2bdfeb9162ef18605a66af984bd88859a7d1694d
federicodonati07 <realeaquila.929@gmail.com>
2024-11-09 22:04:19 +0100

    risolvere require in route.ts di stripe

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const stripe = require("stripe")(process.env.NEXT_PUBLIC_STIRPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 
 export async function POST(request: NextRequest) {
   try {
