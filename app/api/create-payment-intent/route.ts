8b06c22b5ba648e4a1bfc04cd936cb4de4f9e13b
federicodonati07 <realeaquila.929@gmail.com>
2024-11-09 22:07:33 +0100

    risolto problema require

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -1,23 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
 
-const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!);
 
 export async function POST(request: NextRequest) {
   try {
     const { amount } = await request.json();
 
     const paymentIntent = await stripe.paymentIntents.create({
-      amount: amount,
+      amount,
       currency: "usd",
       automatic_payment_methods: { enabled: true },
     });
 
     return NextResponse.json({ clientSecret: paymentIntent.client_secret });
   } catch (error) {
-    console.error("Internal Error: ", error);
+    console.error("Internal Error:", error);
 
     return NextResponse.json(
-      { error: `Internal Server Error: ${error}` },
+      {
+        error: `Internal Server Error: ${
+          error instanceof Error ? error.message : String(error)
+        }`,
+      },
       { status: 500 }
     );
   }
